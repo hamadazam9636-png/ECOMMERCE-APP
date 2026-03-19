8421fd4a3d2b9fb7ed0a5134c39d0a3033895334
CodewithHadi <hamadazam9636@gmail.com>
2026-03-19 03:15:58 -0700

    backend server completed

---
+++ server/routes/ordersRoutes.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import { authorize, protect } from "../middleware/auth.js"
+import { createOrder, getAllOrders, getOrder, getOrders, updateOrderStatus } from "../controllers/ordersController.js"
+
+const OrderRouter = express.Router()
+
+// Get user orders
+OrderRouter.get('/', protect, getOrders)
+
+// Get single order
+OrderRouter.get('/:id', protect, getOrder)
+
+// Create order from cart
+OrderRouter.post('/', protect, createOrder)
+
+// Update order status (Admin Only)
+OrderRouter.put('/:id/status', protect, authorize("admin"), updateOrderStatus)
+
+// Get all orders (Admin Only)
+OrderRouter.get('/admin/all', protect, authorize("admin"), getAllOrders)
+
+export default OrderRouter;
---
+++ server/routes/productsRoutes.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import { getProducts,getProduct, createProduct, updateProduct, deleteProduct } from "../controllers/productController.js"
+import upload from "../middleware/upload.js"
+import { authorize, protect } from "../middleware/auth.js"
+
+const ProductRouter = express.Router()
+
+// Get all products
+ProductRouter.get('/', getProducts)
+
+// Get single product
+ProductRouter.get('/:id', getProduct)
+
+// Create product ( Admin only)
+ProductRouter.post('/', upload.array("images", 5),protect, authorize('admin'), createProduct)
+
+// Update product (Admin only)
+ProductRouter.put('/:id', upload.array("images", 5),protect, authorize('admin'), updateProduct)
+
+// Delete product (Admin only)
+ProductRouter.delete('/:id', protect, authorize('admin'), deleteProduct)
+
+export default ProductRouter;
+
